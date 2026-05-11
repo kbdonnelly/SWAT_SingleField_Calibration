19c0c9c59d7176f302960ea65d52b8dcbf6fb54d
Kevin Donnelly <kbdonnelly99@gmail.com>
2026-05-11 09:41:52 -0400

    Upload of calibration input file

---
+++ NonLatent_CalibFiles/Nominal_Input_Files/000010001.sol
@@ -0,0 +1,21 @@
+ .Sol file Watershed HRU:1 Subbasin:1 HRU:1 Luse:SOYB Soil: 2676641 Slope: 0-9999 1/6/2024 12:00:00 AM ArcSWAT 2012.10_8.26
+ Soil Name: Blount
+ Soil Hydrologic Group: D
+ Maximum rooting depth(mm): 2000.00
+ Porosity fraction from which anions are excluded:0.500
+ Crack volume potential of soil:22.533
+ Texture 1                : SIL- SIC- CL- CL
+ Depth                [mm]:       50.80      152.40      250.00      830.00      990.00     2000.00
+ Bulk Density Moist [g/cc]:        0.98        1.16        1.25        1.26        1.80        1.90
+ Ave. AW Incl. Rock Frag  :        0.21        0.21        0.26        0.28        0.12        0.30
+ Ksat. (est.)      [mm/hr]:       20.32       20.32       20.32       20.32       20.32        2.66
+ Organic Carbon [weight %]:        1.92        1.34        1.16        0.50        0.25        0.25
+ Clay           [weight %]:       27.38       26.65       31.42       34.32       35.00       33.00
+ Silt           [weight %]:       55.36       51.90       49.46       48.32       40.00       45.00
+ Sand           [weight %]:       17.27       21.45       19.11       17.35       25.00       22.00
+ Rock Fragments   [vol. %]:        5.00        5.00        5.00        5.00        1.00        2.00
+ Soil Albedo (Moist)      :        0.29        0.29        0.29        0.29        0.29        0.29
+ Erosion K                :        0.37        0.37        0.37        0.32        0.37        0.37
+ Salinity (EC, Form 5)    :        0.00        0.00        0.00        0.00        0.00        0.00
+ Soil pH                  :        5.60        5.50        6.30        6.50        7.60        8.00
+ Soil CACO3               :        0.00        0.00        0.00        0.00       12.00       24.00
